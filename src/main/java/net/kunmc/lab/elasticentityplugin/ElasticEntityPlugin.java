50d52e8573de42fe4c0d6d2e96b80f6982b1fb7d
maru-256 <maru043@docomo.ne.jp>
2021-11-18 06:19:17 +0900

    add MainCommand

--- src/main/java/net/kunmc/lab/elasticentityplugin/ElasticEntityPlugin.java
+++ src/main/java/net/kunmc/lab/elasticentityplugin/ElasticEntityPlugin.java
@@ -1,11 +1,12 @@
 package net.kunmc.lab.elasticentityplugin;
 
-import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
+import dev.kotx.flylib.FlyLib;
+import net.kunmc.lab.configlib.command.ConfigCommand;
+import net.kunmc.lab.configlib.command.ConfigCommandBuilder;
+import net.kunmc.lab.elasticentityplugin.command.MainCommand;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
-public final class ElasticEntityPlugin extends JavaPlugin implements Listener {
+public final class ElasticEntityPlugin extends JavaPlugin {
     public static Game game;
 
     @Override
@@ -13,12 +14,11 @@ public final class ElasticEntityPlugin extends JavaPlugin implements Listener {
         GameConfig config = new GameConfig(this);
         game = new Game(this, config);
 
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                game.start(Bukkit.getPlayer("Maru256").getLocation());
-            }
-        }.runTaskLater(this, 200);
+        ConfigCommand configCommand = new ConfigCommandBuilder(config).build();
+
+        FlyLib.create(this, builder -> {
+            builder.command(new MainCommand("elastic", configCommand));
+        });
     }
 
     @Override
---
+++ src/main/java/net/kunmc/lab/elasticentityplugin/command/MainCommand.java
@@ -0,0 +1,12 @@
+package net.kunmc.lab.elasticentityplugin.command;
+
+import dev.kotx.flylib.command.Command;
+import net.kunmc.lab.configlib.command.ConfigCommand;
+import org.jetbrains.annotations.NotNull;
+
+public class MainCommand extends Command {
+    public MainCommand(@NotNull String name, ConfigCommand configCommand) {
+        super(name);
+        children(configCommand);
+    }
+}
