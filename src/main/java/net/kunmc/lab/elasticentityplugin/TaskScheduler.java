42df38543e072b3a3b3aab1c1c70fec3f2ceb83a
maru-256 <maru043@docomo.ne.jp>
2021-11-18 04:33:11 +0900

    add TaskScheduler

---
+++ src/main/java/net/kunmc/lab/elasticentityplugin/TaskScheduler.java
@@ -0,0 +1,37 @@
+package net.kunmc.lab.elasticentityplugin;
+
+import net.kunmc.lab.elasticentityplugin.function.Executor;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Deque;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+public class TaskScheduler {
+    private final Deque<BukkitRunnable> deque = new ConcurrentLinkedDeque<>();
+    public int numberOfExecutionsPerSec = 12500;
+
+    public TaskScheduler(Plugin plugin) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < (numberOfExecutionsPerSec / 20) * (Bukkit.getTPS()[0] / 20); i++) {
+                    BukkitRunnable runnable = deque.poll();
+                    if (runnable != null) {
+                        runnable.runTask(plugin);
+                    }
+                }
+            }
+        }.runTaskTimerAsynchronously(plugin, 0, 0);
+    }
+
+    public void offer(Executor executor) {
+        deque.offer(new BukkitRunnable() {
+            @Override
+            public void run() {
+                executor.execute();
+            }
+        });
+    }
+}
